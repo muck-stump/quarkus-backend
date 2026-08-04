8da81a9a25ae874918a0908fa46525fa9e2e8039
muck-stump <202767562+muck-stump@users.noreply.github.com>
2026-08-04 16:13:41 +1000

    Update Post.java
    strange middle ground

--- src/main/java/com/openshift/fordevelopers/Post.java
+++ src/main/java/com/openshift/fordevelopers/Post.java
@@ -24,8 +24,8 @@ public class Post extends PanacheMongoEntity {
     }
 
     public String getTitle() {
-		return title ; 
-        // return new StringBuilder(title).reverse().toString(); // Should be: return title;
+		// return title ; 
+        return new StringBuilder(title).reverse().toString(); // Should be: return title;
     }
 
     public String getContent() {
